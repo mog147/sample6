f27f2a4af2f49125b092af91a6592b4a2001198f
mog147 <396mog@gmail.com>
2025-01-14 11:28:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,69 +1,59 @@
-
-$('.slider').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    centerMode: true,//要素を中央ぞろえにする
-    variableWidth: true,//幅の違う画像の高さを揃えて表示
-    dots: true,//下部ドットナビゲーションの表示
-});
-
-
-$('.slide__full').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-    speed:1000,//スライドの動きのスピード。初期値は300。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 1,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    arrows: true,//左右の矢印あり
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    dots: true,//下部ドットナビゲーションの表示
-    pauseOnFocus: false,//フォーカスで一時停止を無効
-    pauseOnHover: false,//マウスホバーで一時停止を無効
-    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-});
-
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slide__full').on('touchmove', function(event, slick, currentSlide, nextSlide){
-$('.slide__full').slick('slickPlay');
-});
-
-// hamburger
 $(function(){
-    $(".btn-gNav").on("click",function(){
-       $(".gnav").toggleClass("open");
-    });
-  });
 
+    if ($(window).width() <= 768) {
+        // スマホの場合の処理
 
-// 
+                //表示非表示切替
+        $(".header__menu").on("click",function(){
 
+            if ($('.header__menu-bar').css('display') == "none") {
+
+                // 非表示の場合の処理
+                $("#target").addClass("fixed");      //id[target]を固定
+                $('.header__menu-bar').slideToggle();//menu-barを開く
+
+            } else {
+
+                // 表示の場合の処理
+                $("#target").removeClass("fixed");//id[target]を固定解除
+                $('.header__menu-bar').hide();    //menu-barを閉じる
+        
+            }
+        })
+    }
+
+    
+});
 
 $(function(){
 
-    //表示非表示切替
-    $(".more-btn").on("click",function(){
+    if ($(window).width() <= 768) {
+        // スマホの場合の処理
 
-        if ($('#tgt').hasClass('open')){
+                //表示非表示切替
+        $(".header__menu-bar").on("click",function(){
 
-            // 非表示の場合の処理
-            $(".conF").css({'padding-bottom':'0rem'});
-            $('#tgt').removeClass('open');
+            if ($('.header__menu-bar').css('display') == "none") {
 
-        } else {
+                // 非表示の場合の処理
+                $("#target").addClass("fixed");
+                $('.header__menu-bar').slideToggle();
 
-            // 表示の場合の処理
-            $('#tgt').addClass('open');
-            $(".conF").css({'padding-bottom':'50rem'});
-    
-        }
-    })
+            } else {
+
+                // 表示の場合の処理
+                $("#target").removeClass("fixed");
+                $('.header__menu-bar').hide();
+        
+            }
+        })
+    }
+
+
     
 });
+
+
+
+
 
